c6a159c798539d739fd0194a30500219a2856ec7
Dominic van Almsick <dominicvana@gmail.com>
2023-07-27 11:53:26 +0100

    chore: init auth integration

---
+++ lib/auth.ts
@@ -0,0 +1,20 @@
+import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from './db';
+import type { NextAuthOptions } from 'next-auth';
+
+export const authOptions: NextAuthOptions = {
+  // @ts-ignore
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+};
