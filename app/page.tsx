879c8a27023073d0cf3b92505dd266c331c82b32
Dominic van Almsick <dominicvana@gmail.com>
2023-07-28 12:14:27 +0100

    fix: pass isLoggedIn prop to header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import { isLoggedIn } from '@/lib/user';
 import Header from '@/components/layout/Header';
 import Hero from '@/components/layout/Hero';
 import Container from '@/components/utils/Container';
 
-export default function Home() {
+export default async function Home() {
   return (
     <Container className="px-4">
-      <Header />
+      <Header isLoggedIn={await isLoggedIn()} />
       <main>
         <Hero />
       </main>
