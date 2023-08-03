68858cd8d2308cb077fdd2ef4d9147f4da7de4e2
Dominic van Almsick <dominicvana@gmail.com>
2023-08-03 12:19:31 +0100

    fix: JournalEntryCard layout

--- components/journal/JournalEntryCard.tsx
+++ components/journal/JournalEntryCard.tsx
@@ -4,7 +4,11 @@ import type { JournalEntry } from '@prisma/client';
 
 export default function JournalEntryCard({ entry }: { entry: JournalEntry }) {
   return (
-    <Card as="li" key={entry.id} className="h-32 basis-1/4 p-0">
+    <Card
+      as="li"
+      key={entry.id}
+      className="h-32 basis-[45%] p-0 sm:basis-[30%]"
+    >
       <Link
         href={`/dashboard/journal/${entry.id}`}
         className="flex h-full w-full items-center justify-center"
