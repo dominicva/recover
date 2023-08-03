b944aba881b4f18e6cc991fa837e097bee7e0ed1
Dominic van Almsick <dominicvana@gmail.com>
2023-08-03 15:02:37 +0100

    fix: correct env variables

--- app/api/ai/route.ts
+++ app/api/ai/route.ts
@@ -4,5 +4,6 @@ import { NextResponse, type NextRequest } from 'next/server';
 export const POST = async (request: NextRequest) => {
   const { userContent } = await request.json();
   const response = await openAI(userContent);
+
   return NextResponse.json({ data: response.data.choices[0].message?.content });
 };
