5de92daaad32b8a6dd8cc3d05b544f526e679dca
Dominic van Almsick <dominicvana@gmail.com>
2023-09-01 15:10:13 +0100

    refactor: footer to nav

---
+++ components/layout/Nav.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import { Icons } from '../ui/icons';
+
+export default function Nav() {
+  return (
+    <div className="sticky -bottom-1">
+      <nav className="rounded-lg bg-gray-100 py-6">
+        <ul className="flex justify-center gap-14">
+          <li>
+            <Link href="/dashboard">
+              <Icons.home />
+            </Link>
+          </li>
+          <li>
+            <Link href="/dashboard/journal">
+              <Icons.alignLeft />
+            </Link>
+          </li>
+          <li>
+            <Link href="/dashboard/progress">
+              <Icons.barChart />
+            </Link>
+          </li>
+          <li>
+            <Link href="/dashboard/achievements">
+              <Icons.trophy />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
