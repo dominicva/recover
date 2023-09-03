4b35ebe367876a6d6464c60683f285c4e6f2bc30
Dominic van Almsick <dominicvana@gmail.com>
2023-09-03 12:17:32 +0100

    fix: escape apostrophe

--- components/profile/SettingsForm.tsx
+++ components/profile/SettingsForm.tsx
@@ -132,7 +132,8 @@ export default function ProfileSettings({
           <DialogHeader>
             <DialogTitle>Edit profile</DialogTitle>
             <DialogDescription>
-              Make changes to your profile here. Click save when you're done.
+              Make changes to your profile here. Click save when you&apos;re
+              done.
             </DialogDescription>
           </DialogHeader>
           <Form {...form}>
